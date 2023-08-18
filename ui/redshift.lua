81abd094058aed3a03dffcdef2724f8fbd93674a
Tracktark <rikoczz@gmail.com>
2023-08-18 08:46:49 +0200

    Added sunset based redshift activation

---
+++ ui/redshift.lua
@@ -0,0 +1,15 @@
+local darkmode = require "lib.darkmode"
+local redshift = require "system.redshift"
+
+local rs = darkmode {
+   location = {
+      lat = 49.1521,
+      long = 18.749,
+   }
+}
+
+rs:connect_signal(function(active)
+   redshift.active = active
+end)
+
+rs:start()
