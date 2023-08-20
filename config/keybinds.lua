4db63760d34c39709d3d825f09e87a99035e749d
Tracktark <rikoczz@gmail.com>
2023-08-20 10:34:36 +0200

    Made tags remember on which screen they are over restarts

--- config/keybinds.lua
+++ config/keybinds.lua
@@ -3,35 +3,36 @@ local brightness = require "system.brightness"
 local audio = require "system.audio"
 local sharedtags = require "lib.sharedtags"
 local darkmode = require "ui.darkmode"
+local tags = require "config.tags"
 
 local key = awful.key
 local modkey = "Mod4"
 
 awful.keyboard.append_global_keybindings { group = "tag",
-  key({modkey}, "Tab", awful.tag.history.restore, {description = "Go to last tag"}),
-  key({modkey}, "c", function()
+   key({ modkey }, "Tab", awful.tag.history.restore, { description = "Go to last tag" }),
+   key({ modkey }, "c", function()
       if screen:count() < 2 then return end
       local last_tag = screen[1].selected_tag
       for i = 2, screen:count() do
-        local curr_tag = screen[i].selected_tag
-        if last_tag then
-          sharedtags.viewonly(last_tag, screen[i])
-        end
-        last_tag = curr_tag
+         local curr_tag = screen[i].selected_tag
+         if last_tag then
+            tags.view_tag(last_tag, screen[i])
+         end
+         last_tag = curr_tag
       end
       if last_tag then
-        sharedtags.viewonly(last_tag, screen[1])
+         tags.view_tag(last_tag, screen[1])
       end
-    end, {description = "Rotate tags on screens"}),
-key({modkey, "Shift"}, "c", function()
+   end, { description = "Rotate tags on screens" }),
+   key({ modkey, "Shift" }, "c", function()
       local screen_count = screen.count()
       if screen_count < 2 then return end
       local focused = awful.screen.focused()
       local new_screen_index = (focused.index % screen_count) + 1
-      sharedtags.viewonly(focused.selected_tag, screen[new_screen_index])
-end, {descripiton = "Move tag to another screen"}),
-key({modkey}, "s", function() awful.screen.focus_relative(1) end,
-    {description = "Move focus to next screen"}),
+      tags.view_tag(focused.selected_tag, screen[new_screen_index])
+   end, { descripiton = "Move tag to another screen" }),
+   key({ modkey }, "s", function() awful.screen.focus_relative(1) end,
+      { description = "Move focus to next screen" }),
 }
 
 for i = 1, 9 do
@@ -56,27 +57,27 @@ for i = 1, 9 do
 end
 
 awful.keyboard.append_global_keybindings { group = "launcher",
-  key({modkey}, "Return", function() awful.spawn("alacritty") end,                       {description = "Open a terminal"}),
-  key({modkey}, ";",      function() awful.spawn("emacsclient -nc") end,                 {description = "Open emacsclient"}),
-  key({modkey}, "e",      function() awful.spawn("thunar") end,                          {description = "Open file browser"}),
-  key({modkey}, "d",      function() awful.spawn("rofi -show combi -normal-window") end, {description = "Open Rofi"}),
-  key({modkey}, "z",      function() awful.spawn("boomer") end,                          {description = "Zoom in"}),
+   key({ modkey }, "Return", function() awful.spawn("alacritty") end, { description = "Open a terminal" }),
+   key({ modkey }, ";", function() awful.spawn("emacsclient -nc") end, { description = "Open emacsclient" }),
+   key({ modkey }, "e", function() awful.spawn("thunar") end, { description = "Open file browser" }),
+   key({ modkey }, "d", function() awful.spawn("rofi -show combi -normal-window") end, { description = "Open Rofi" }),
+   key({ modkey }, "z", function() awful.spawn("boomer") end, { description = "Zoom in" }),
 }
 
 awful.keyboard.append_global_keybindings { group = "awesome",
-  key({modkey, "Ctrl"}, "r", awesome.restart, {description = "Reload awesome"}),
-  key({modkey, "Ctrl"}, "x", awesome.quit, {description = "Close awesome"}),
+   key({ modkey, "Ctrl" }, "r", awesome.restart, { description = "Reload awesome" }),
+   key({ modkey, "Ctrl" }, "x", awesome.quit, { description = "Close awesome" }),
 
-  key({modkey}, "t", function()
-        darkmode:toggle()
-  end, {description = "Toggle color scheme"}),
+   key({ modkey }, "t", function()
+      darkmode:toggle()
+   end, { description = "Toggle color scheme" }),
 }
 
 awful.keyboard.append_global_keybindings { group = "layout",
-  key({modkey}, "Up", function() awful.layout.inc(1) end,
-    {description = "Use next layout style"}),
-  key({modkey}, "Down", function() awful.layout.inc(-1) end,
-    {description = "Use previous layout style"}),
+   key({ modkey }, "Up", function() awful.layout.inc(1) end,
+      { description = "Use next layout style" }),
+   key({ modkey }, "Down", function() awful.layout.inc(-1) end,
+      { description = "Use previous layout style" }),
 }
 
 awful.keyboard.append_client_keybindings { group = "client",
@@ -90,13 +91,13 @@ awful.keyboard.append_client_keybindings { group = "client",
 -- Brightness
 awful.keyboard.append_global_keybindings { group = "system",
    awful.key({}, "XF86MonBrightnessDown", function() brightness.add(awful.screen.focused(), -5) end),
-   awful.key({}, "XF86MonBrightnessUp",   function() brightness.add(awful.screen.focused(),  5) end),
+   awful.key({}, "XF86MonBrightnessUp", function() brightness.add(awful.screen.focused(), 5) end),
 }
 
 -- Volume
 awful.keyboard.append_global_keybindings { group = "system",
    awful.key({}, "XF86AudioLowerVolume", function() audio.add(-5) end),
-   awful.key({}, "XF86AudioRaiseVolume", function() audio.add( 5) end),
+   awful.key({}, "XF86AudioRaiseVolume", function() audio.add(5) end),
    awful.key({}, "XF86AudioMute", function() audio.toggle_mute() end),
 }
 
--- config/tags.lua
+++ config/tags.lua
@@ -1,9 +1,15 @@
 local sharedtags = require "lib.sharedtags"
 local awful = require "awful"
+local db = require "system.db"
+local gears = require "gears"
+
+db:exec [[
+CREATE TABLE IF NOT EXISTS tags (name TEXT UNIQUE, screen_index INTEGER);
+]]
 
 local l = awful.layout.suit
 
-sharedtags {
+local tags = sharedtags {
       { name = "www", layout = l.tile },
       { name = "emacs", layout = l.max },
       { name = "term", layout = l.tile },
@@ -11,4 +17,30 @@ sharedtags {
       { name = "chat", layout = l.max },
       { name = "game", layout = l.tile },
       { name = "notes", layout = l.tile },
+}
+
+for res in db:nrows("SELECT name, screen_index FROM tags;") do
+   local tag = tags[res.name]
+
+   if tag ~= nil and res.screen_index <= screen:count() then
+      sharedtags.viewonly(tag, screen[res.screen_index])
+   end
+end
+
+local function view_tag(tag, screen)
+   sharedtags.viewonly(tag, screen)
+   local stmt = db:prepare [[
+   INSERT INTO tags (name, screen_index) VALUES (:tag, :screen)
+   ON CONFLICT(name) DO UPDATE SET screen_index=excluded.screen_index;
+   ]]
+   stmt:bind_names {
+      tag = tag.name,
+      screen = screen.index,
+   }
+   stmt:step()
+   stmt:finalize()
+end
+
+return {
+   view_tag = view_tag
 }
