f473f5d9c83d639e5b8e127fa4b89dc660155cae
Tracktark <rikoczz@gmail.com>
2023-09-01 22:51:02 +0200

    Added toggling of dashboard with top mouse button

--- config/mousebinds.lua
+++ config/mousebinds.lua
@@ -2,13 +2,22 @@ local awful = require "awful"
 
 local button = awful.button.names
 awful.mouse.append_client_mousebindings {
-    awful.button({ }, button.LEFT, function (c)
-        c:activate { context = "mouse_click", raise = true }
-    end),
-    awful.button({ "Mod4" }, button.LEFT, function (c)
-        c:activate { context = "mouse_click", raise = true, action = "mouse_move" }
-    end),
-    awful.button({ "Mod4" }, button.RIGHT, function (c)
-        c:activate { context = "mouse_click", raise = true, action = "mouse_resize" }
-    end),
+   awful.button({}, button.LEFT, function(c)
+      c:activate { context = "mouse_click", raise = true }
+   end),
+   awful.button({ "Mod4" }, button.LEFT, function(c)
+      c:activate { context = "mouse_click", raise = true, action = "mouse_move" }
+   end),
+   awful.button({ "Mod4" }, button.RIGHT, function(c)
+      c:activate { context = "mouse_click", raise = true, action = "mouse_resize" }
+   end),
+   awful.button({}, 10, function()
+         awful.screen.focused().dashboard:toggle()
+   end)
+}
+
+awful.mouse.append_global_mousebindings {
+   awful.button({}, 10, function()
+         awful.screen.focused().dashboard:toggle()
+   end)
 }
