ad289999777cb2f1a9f59891faa08f8f4116dc69
Tracktark <rikoczz@gmail.com>
2023-09-01 22:50:06 +0200

    Moved database into the awesome data folder

--- system/db.lua
+++ system/db.lua
@@ -1,6 +1,6 @@
 local sqlite = require "lsqlite3"
 local fs = require("gears").filesystem
 
-local db = sqlite.open(fs.get_xdg_data_home() .. "awesome.db")
+local db = sqlite.open(fs.get_xdg_data_home() .. "awesome/awesome.db")
 
 return db
